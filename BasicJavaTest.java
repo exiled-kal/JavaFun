feb8f7c80769eb4abccf80de4c10cf50c64e676e
kaldne <tseringkalden1964@gmail.com>
2020-04-06 22:32:25 -0700

    started doing basicjavatest

---
+++ BasicJavaTest.java
@@ -0,0 +1,13 @@
+
+public class BasicJavaTest {
+    public static void main(String[] args) {
+    BasicJava testNumber = new BasicJava();
+
+    testNumber.print1To255();
+
+    testNumber.printOdd1To255();
+
+    testNumber.printSum();
+
+        }
+}
