ec392df14857c655a7dd74f8caff5ea947e02333
kaldne <tseringkalden1964@gmail.com>
2020-04-07 11:55:57 -0700

    added changes

--- BasicJava.java
+++ BasicJava.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 public class BasicJava{
 
     public static void main(String args[]){
+
+    public void print1to255(){
         for(int i = 1; i < 256; i++){
             System.out.println(i);
         }
--- BasicJavaTest.java
+++ BasicJavaTest.java
@@ -1,13 +1,40 @@
 
 public class BasicJavaTest {
     public static void main(String[] args) {
-    BasicJava testNumber = new BasicJava();
+    BasicJava testRun = new BasicJava();
 
-    testNumber.print1To255();
+    testRun.print1to255();
 
-    testNumber.printOdd1To255();
+    testRun.printOdd1To255();
 
-    testNumber.printSum();
+    testRun.printSum();
 
-        }
+    int[] testArr = {};
+    testRun.iterateArray(testArr);
+
+    int[] testArrRun = {};
+    testRun.findMax(testArrRun);
+
+    int[] testArrRun1 = {};
+    testRun.getAverage(testArrRun);
+
+    int[] testArrRun2 = {};
+    testRun.arrayOddNumbers();
+
+    int[] testArrRun3 = {};
+    testRun.greaterThanY(testArrRun, );
+
+    int[] testArrRun4 = {};
+    testRun.squareTheValues(testArrRun);
+
+    int[] testArrRun5 = {};
+    testRun.eliminateNegativeNumbers(testArrRun);
+
+    int[] testArrRun6 = {};
+    testRun.maxMinAvg(testArr);
+
+    int[] testArrRun7 = {};
+    testRun.shiftingValues(testArrRun);
+
+   }
 }
