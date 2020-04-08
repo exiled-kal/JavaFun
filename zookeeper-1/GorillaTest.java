852bdc2a08252ec8b9e9815678332e32db6701ad
kaldne <tseringkalden1964@gmail.com>
2020-04-08 16:12:44 -0700

    commit

--- zookeeper-1/GorillaTest.java
+++ zookeeper-1/GorillaTest.java
@@ -4,9 +4,9 @@ public class GorillaTest {
     public static void main(String[] args) {
 		Gorilla gorilla = new Gorilla();
 		gorilla.throwSomething();
-		gorilla.throwSomething();
-		gorilla.throwSomething();
-		gorilla.eatBananas();
+		
+		
+		
 		gorilla.eatBananas();
 		gorilla.climb();
 		
