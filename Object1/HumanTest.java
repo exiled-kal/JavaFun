71609b92dfb4a11099f7d5d519b6d6fa21a2064c
kaldne <tseringkalden1964@gmail.com>
2020-04-09 11:29:25 -0700

    added

--- Object1/HumanTest.java
+++
@@ -1,8 +0,0 @@
-
-public class HumanTest {
-    public static void main(String[] args){
-        Human a = new Human ("kalden");
-        Human b = new Human ("tashi");
-        a.attack(b);
-    }
-}
